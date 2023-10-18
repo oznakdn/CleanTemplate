b9938dc88b606de7b22f1b9cf6eec3c1cc6f8d3b
oznakdn <79724084+oznakdn@users.noreply.github.com>
2023-10-18 12:21:07 +0300

    EFRepository updated

--- src/Core/Clean.Domain.Contracts/Interfaces/IEFRepository.cs
+++ src/Core/Clean.Domain.Contracts/Interfaces/IEFRepository.cs
@@ -6,15 +6,24 @@ public interface IEFRepository<TEntity, TId>:IUnitOfWork
 where TEntity : class, IEntity<TId>
 {
     void Insert(TEntity entity);
+    Task<TEntity> InsertAsync(TEntity entity, CancellationToken cancellationToken);
+
     void Update(TEntity entity);
+    Task<TEntity> UpdateAsync(TEntity entity, CancellationToken cancellationToken);
+
     void Delete(TEntity entity);
+    Task<TEntity?> DeleteAsync(TEntity entity, CancellationToken cancellationToken);
 
     IQueryable<TEntity> GetQueryable();
 
     Task<IEnumerable<TEntity>> GetAllAsync(CancellationToken cancellation, Expression<Func<TEntity, bool>> predicate = null, params Expression<Func<TEntity, object>>[] includeProperties);
 
+    IEnumerable<TEntity> GetAll(Expression<Func<TEntity, bool>> predicate = null, params Expression<Func<TEntity, object>>[] includeProperties);
+
     Task<TEntity> GetAsync(CancellationToken cancellation, Expression<Func<TEntity, bool>> predicate, params Expression<Func<TEntity, object>>[] includeProperties);
 
+    TEntity GetAsync(Expression<Func<TEntity, bool>> predicate, params Expression<Func<TEntity, object>>[] includeProperties);
+    int Save();
 
     //TODO: Pagination ile ilgili metot eklenecek
 }
--- src/Infrastructure/Clean.Persistence/Repositories/Common/EFRepository.cs
+++ src/Infrastructure/Clean.Persistence/Repositories/Common/EFRepository.cs
@@ -3,7 +3,7 @@
 namespace Clean.Persistence.Repositories.Common;
 
 public abstract class EFRepository<TEntity, TContext, TId> : IEFRepository<TEntity, TId>
-where TEntity : class,IEntity<TId>
+where TEntity : class, IEntity<TId>
 where TContext : DbContext
 {
     protected readonly TContext _dbContext;
@@ -14,24 +14,57 @@ where TContext : DbContext
         _table = _dbContext.Set<TEntity>();
     }
 
-    public virtual void Delete(TEntity entity)
-    {
-        _table.Remove(entity);
-    }
+    #region Insert
 
     public virtual void Insert(TEntity entity)
     {
         _table.Add(entity);
     }
 
+    public virtual async Task<TEntity> InsertAsync(TEntity entity, CancellationToken cancellationToken)
+    {
+        await _table.AddAsync(entity, cancellationToken);
+        return entity;
+    }
+
+    #endregion
+
+    #region Update
     public virtual void Update(TEntity entity)
     {
         _table.Update(entity);
     }
+    public async Task<TEntity> UpdateAsync(TEntity entity, CancellationToken cancellationToken)
+    {
+        await Task.Run(() => _table.Update(entity), cancellationToken);
+        return entity;
+    }
 
-    public virtual async Task<IEnumerable<TEntity>> GetAllAsync(
-        CancellationToken cancellationToken, 
-        Expression<Func<TEntity, bool>> predicate = null, 
+    #endregion
+
+    #region Delete
+
+    public virtual void Delete(TEntity entity)
+    {
+        _table.Remove(entity);
+    }
+
+    public async Task<TEntity?> DeleteAsync(TEntity entity, CancellationToken cancellationToken)
+    {
+        await Task.Run(() => _table.Remove(entity), cancellationToken);
+        return entity;
+    }
+
+    #endregion
+
+    #region Save
+    public async Task<int> SaveAsync(CancellationToken cancellationToken = default) => await _dbContext.SaveChangesAsync(cancellationToken);
+    public int Save() => _dbContext.SaveChanges();
+
+    #endregion
+
+
+    public IEnumerable<TEntity> GetAll(Expression<Func<TEntity, bool>> predicate = null, 
         params Expression<Func<TEntity, object>>[] includeProperties)
     {
         IQueryable<TEntity> _query = _table;
@@ -45,12 +78,29 @@ where TContext : DbContext
             }
         }
 
-        return await _query.ToListAsync(cancellationToken);
+        return _query.AsNoTracking().ToList();
     }
 
-    public virtual async Task<TEntity> GetAsync(
-        CancellationToken cancellation, 
-        Expression<Func<TEntity, bool>> predicate, 
+
+    public virtual async Task<IEnumerable<TEntity>> GetAllAsync(CancellationToken cancellationToken,
+        Expression<Func<TEntity, bool>> predicate = null,
+        params Expression<Func<TEntity, object>>[] includeProperties)
+    {
+        IQueryable<TEntity> _query = _table;
+        _query = predicate != null ? _query.Where(predicate) : _query;
+
+        if (includeProperties.Length > 0)
+        {
+            foreach (var property in includeProperties)
+            {
+                _query = _query.Include(property);
+            }
+        }
+
+        return await _query.AsNoTracking().ToListAsync(cancellationToken);
+    }
+
+    public TEntity GetAsync(Expression<Func<TEntity, bool>> predicate, 
         params Expression<Func<TEntity, object>>[] includeProperties)
     {
         IQueryable<TEntity> _query = _table;
@@ -63,13 +113,30 @@ where TContext : DbContext
                 _query = _query.Include(property);
             }
         }
-        return await _query.SingleOrDefaultAsync(cancellation);
+        return _query.AsNoTracking().SingleOrDefault();
+    }
+
+    public virtual async Task<TEntity> GetAsync(CancellationToken cancellation,
+        Expression<Func<TEntity, bool>> predicate,
+        params Expression<Func<TEntity, object>>[] includeProperties)
+    {
+        IQueryable<TEntity> _query = _table;
+        _query = _query.Where(predicate);
+
+        if (includeProperties.Length > 0)
+        {
+            foreach (var property in includeProperties)
+            {
+                _query = _query.Include(property);
+            }
+        }
+        return await _query.AsNoTracking().SingleOrDefaultAsync(cancellation);
     }
 
     public virtual IQueryable<TEntity> GetQueryable() => _table.AsQueryable();
 
-    public async Task<int> SaveAsync(CancellationToken cancellationToken = default) => await _dbContext.SaveChangesAsync(cancellationToken);
 
-    public async ValueTask DisposeAsync() =>await _dbContext.DisposeAsync();
-  
+    public async ValueTask DisposeAsync() => await _dbContext.DisposeAsync();
+
+   
 }
