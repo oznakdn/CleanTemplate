377a597402837436683fa63cc9d4d0991bceb52b
oznakdn <79724084+oznakdn@users.noreply.github.com>
2023-10-23 14:21:10 +0300

    Specification feauture was beeen added

---
+++ src/Core/Clean.Domain.Contracts/Abstracts/Specification.cs
@@ -0,0 +1,15 @@
+﻿using Clean.Domain.Contracts.Interfaces;
+using System.Linq.Expressions;
+
+namespace Clean.Domain.Contracts.Abstracts;
+
+public abstract class Specification<TEntity> : ISpecification<TEntity>
+{
+    public abstract Expression<Func<TEntity, bool>> ToExpression();
+
+    public bool IsSatisfiedBy(TEntity entity)
+    {
+        Func<TEntity, bool> predicate = ToExpression().Compile();
+        return predicate(entity);
+    }
+}
---
+++ src/Core/Clean.Domain.Contracts/Interfaces/ISpecification.cs
@@ -0,0 +1,6 @@
+﻿namespace Clean.Domain.Contracts.Interfaces;
+
+public interface ISpecification<TEntity>
+{
+    bool IsSatisfiedBy(TEntity entity);
+}
