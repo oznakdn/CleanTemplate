299321c41ce1fd5dacb4ac8c60e7c78afe8c8cf3
oznakdn <79724084+oznakdn@users.noreply.github.com>
2023-11-01 10:51:03 +0300

    Order tests were done

---
+++ test/UnitTests/DomainTests/OrderTests.cs
@@ -0,0 +1,35 @@
+using Clean.Domain.Orders;
+using Clean.Domain.Shared;
+
+namespace DomainTests;
+
+public class OrderTests
+{
+
+    Guid CustomerId {get;set;} = Guid.NewGuid();
+    Guid ProductId {get;set;} = Guid.NewGuid();
+
+    [Fact]
+    public void CreateOrder_Should_Not_Be_Null()
+    {
+        var order = new Order(CustomerId);
+        Assert.NotNull(order);
+    }
+
+    [Fact]
+    public void AddOrderItem_ShouldBe_Return_Successed()
+    {
+        var order = new Order(CustomerId);
+        TResult<OrderItem> orderResult = order.AddOrderItem(ProductId,1);
+        Assert.True(orderResult.IsSuccessed);
+        Assert.IsType(typeof(Guid),orderResult.Value.OrderId);
+    }
+
+     [Fact]
+    public void AddOrderIte_When_QuantityEqualOrLess_Than_Zero_ShouldBe_Return_Failed()
+    {
+        var order = new Order(CustomerId);
+        TResult<OrderItem> orderResult = order.AddOrderItem(ProductId,-1);
+        Assert.True(orderResult.IsFailed);
+    }
+}
