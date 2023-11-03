e2542e91e9d9998e66972d3688314c84931c3fc5
oznakdn <79724084+oznakdn@users.noreply.github.com>
2023-11-03 17:12:48 +0300

    Application tests have been created

---
+++ test/UnitTests/ApplicationTests/FeatureTests/CustomerQueryTests.cs
@@ -0,0 +1,37 @@
+﻿using Clean.Application.Features.Customers.Queries.GetCustomers;
+using Clean.Application.UnitOfWork.Queries;
+using Clean.Domain.Customers;
+using Moq;
+
+namespace ApplicationTests.FeatureTests;
+
+public class CustomerQueryTests
+{
+
+    private readonly Mock<IQueryUnitOfWork> _moq;
+    private readonly GetCustomersHandler _customersHandler;
+    public CustomerQueryTests()
+    {
+        _moq = new Mock<IQueryUnitOfWork>();
+        _customersHandler = new GetCustomersHandler(_moq.Object);
+    }
+
+
+    [Fact]
+    public void GetCustomers_ShouldBe_Return_Success()
+    {
+        var result = Customer.CreateCustomer("TestName", "TestSurname", "test@mail.com", "5001002030", "123456");
+        Customer customer = result.Value;
+
+        _moq.Setup(m => m.Customer.ReadSingle(true, x => x.Id == result.Value.Id)).Returns(customer);
+        var handle = _customersHandler.Handle(new GetCustomersRequest(50, 1, 1), CancellationToken.None).Result;
+
+        GetCustomersResponse actualResult = handle.Value;
+
+        GetCustomersResponse expectedResult = new(customer.Id.ToString(), customer.FirstName, customer.LastName, customer.Email, customer.PhoneNumber);
+
+
+        Assert.Equal<string>(expectedResult.Id, actualResult.Id);
+
+    }
+}
---
+++ test/UnitTests/ApplicationTests/FeatureTests/ProductQueryTests.cs
@@ -0,0 +1,47 @@
+﻿using Clean.Application.Features.Products.Queries.GetProducts;
+using Clean.Application.UnitOfWork.Queries;
+using Clean.Domain.Products;
+using Clean.Domain.Shared;
+using Moq;
+
+namespace ApplicationTests.FeatureTests;
+
+public class ProductQueryTests
+{
+    private readonly GetProductsHandler _getProductsHandler;
+    private readonly Mock<IQueryUnitOfWork> _moq;
+    public ProductQueryTests()
+    {
+        _moq = new Mock<IQueryUnitOfWork>();
+        _getProductsHandler = new GetProductsHandler(_moq.Object);
+
+    }
+
+    [Fact]
+    public async Task GetProducts_ShouldBe_Returns_Values()
+    {
+        var products = new List<Product>();
+        Product product = new("TestProduct");
+        product.AddMoney(Currency.TL, 1000);
+        product.AddCategory("TestCategory");
+        product.AddInventory(product.Id, 100);
+        products.Add(product);
+
+        List<GetProductsResponse> dto = products.Select(p => new GetProductsResponse(
+            p.Id.ToString(), 
+            p.DisplayName, 
+            p.Price.Currency.ToString(), 
+            p.Price.Amount, 
+            p.Category.DisplayName)).ToList();
+        
+
+         _moq.Setup(x => x.Product.GetAllProductsWithInventoryAsync(default).Result).Returns(products);
+
+        TResult<GetProductsResponse> result = await _getProductsHandler.Handle(new GetProductsRequest(), default);
+
+        Assert.True(result.IsSuccessed);
+        Assert.Equal<GetProductsResponse>(dto, result.Values);
+
+
+    }
+}
