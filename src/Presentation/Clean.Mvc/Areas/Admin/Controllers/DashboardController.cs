82b5a1934e4ced8a469665bcf11b80ae78872015
ozan <ozanakaydin@gmail.com>
2024-01-06 14:43:56 +0300

    Mvc project - updated

---
+++ src/Presentation/Clean.Mvc/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Clean.Mvc.Areas.Admin.Controllers;
+
+public class DashboardController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
--- src/Presentation/Clean.Mvc/Program.cs
+++ src/Presentation/Clean.Mvc/Program.cs
@@ -23,6 +23,12 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+app.MapAreaControllerRoute(
+    name: "Admin", 
+    areaName: "Admin", 
+    pattern: "Admin/{controller=Dashboard}/{action=Index}/{id?}");
+
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
