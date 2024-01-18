10709331a6c586f10e70b941af601919cf7f67e4
ozan <ozanakaydin@gmail.com>
2024-01-18 23:14:09 +0300

    Auth - Register method refactoring

--- src/Core/Clean.Application/Features/Users/Commands/Register/RegisterHandler.cs
+++ src/Core/Clean.Application/Features/Users/Commands/Register/RegisterHandler.cs
@@ -6,49 +6,53 @@ using Clean.Shared;
 namespace Clean.Application.Features.Users.Commands.Register;
 
 
-public record RegisterRequest(string FirstName, string LastName, string Username, string Email, string Password, string? RoleId) : IRequest<IResult<RegisterResponse>>;
-public record RegisterResponse;
+public record RegisterRequest(string FirstName, string LastName, string Username, string Email, string Password, string? RoleId) : IRequest<IResult>;
 
-public class RegisterHandler : IRequestHandler<RegisterRequest, IResult<RegisterResponse>>
+public class RegisterHandler : IRequestHandler<RegisterRequest, IResult>
 {
 
     private readonly IUserCommand _command;
-
-    public RegisterHandler(IUserCommand command)
+    private readonly IUserQuery _query;
+    public RegisterHandler(IUserCommand command, IUserQuery query)
     {
         _command = command;
+        _query = query;
     }
 
-    public async Task<IResult<RegisterResponse>> Handle(RegisterRequest request, CancellationToken cancellationToken)
+    public async Task<IResult> Handle(RegisterRequest request, CancellationToken cancellationToken)
     {
+
         var validator = new RegisterValidator();
         var validation = validator.Validate(request);
         var errors = new List<string>();
-        IResult<User> result;
 
         if (!validation.IsValid)
         {
             validation.Errors.ForEach(error => errors.Add(error.ErrorMessage));
-            return Result<RegisterResponse>.Fail(errors: errors);
+            return Result.Fail(errors: errors);
         }
+
+
+        User? existUser = await _query.ReadFirstOrDefaultAsync(x => x.Email == request.Email);
+
+        if (existUser is not null)
+            return Result.Fail("You cannot that entered email address!");
+        
+
+        IResult<User> result;
 
         if (string.IsNullOrEmpty(request.RoleId))
-        {
             result = User.CreateUser(request.FirstName, request.LastName, request.Username, request.Email, request.Password.HashPassword());
 
-        }
         else
-        {
             result = User.CreateUser(request.FirstName, request.LastName, request.Username, request.Email, request.Password.HashPassword(), request.RoleId);
-        }
+
 
         if (!result.IsSuccess)
-        {
-            return Result<RegisterResponse>.Fail(errors: errors);
-        }
+            return Result.Fail(errors: errors);
+        
 
         await _command.CreateAsync(result.Value, cancellationToken);
-        return Result<RegisterResponse>.Success("User was be register.");
+        return Result.Success("Registration is successfully.");
     }
-
 }
